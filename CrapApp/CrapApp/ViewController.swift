94bf51e342ddf234391321393c584143fbdeb17d
Akash Bhavsar <akash.bhavsar@willdooit.com>
2019-04-11 19:55:18 +1000

    My first crap app

---
+++ CrapApp/CrapApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CrapApp
+//
+//  Created by Akash Bhavsar on 11/4/19.
+//  Copyright © 2019 Akash Bhavsar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func DisplayCrap() {
+        print("I am Crap App");
+    }
+
+}
+
